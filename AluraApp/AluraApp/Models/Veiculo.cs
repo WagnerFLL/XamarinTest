b54182a3170bba7f598388145c8cf39f8694e974
Wagner Fontes <wsf@ic.ufal.br>
2018-01-29 18:05:59 -0200

    Erro na View Agendamento

--- AluraApp/AluraApp/Models/Veiculo.cs
+++ AluraApp/AluraApp/Models/Veiculo.cs
@@ -8,6 +8,30 @@ namespace AluraApp.Models
     {
         public decimal Preco { get; set; }
         public string Modelo { get; set; }
+        public decimal FreioABSValue { get; set; }
+        public decimal MP3PlayerValue { get; set; }
+        public decimal ArCondicionadoValue { get; set; }
+        public string ArCondicionado
+        {
+            get
+            {
+                return String.Format("Ar Condicionado - R$ {0}", this.ArCondicionadoValue);
+            }
+        }
+        public string FreioABS
+        {
+            get
+            {
+                return String.Format("Ar Condicionado - R$ {0}", this.FreioABSValue);
+            }
+        }
+        public string MP3Player
+        {
+            get
+            {
+                return String.Format("MP3Player - R$ {0}", this.MP3PlayerValue);
+            }
+        }
         public string PrecoStr
         {
             get
@@ -19,5 +43,13 @@ namespace AluraApp.Models
         {
             return this.Modelo;
         }
+
+        public Veiculo()
+        {
+            this.FreioABSValue = 1200;
+            this.MP3PlayerValue = 785;
+            this.ArCondicionadoValue = 2300;
+        }
+
     }
 }
--- AluraApp/AluraApp/Views/DetalheView.xaml.cs
+++ AluraApp/AluraApp/Views/DetalheView.xaml.cs
@@ -14,11 +14,39 @@ namespace AluraApp.Views
 	public partial class DetalheView : ContentPage
 	{
         public Veiculo Choice { get; set; }
+        public bool TemFreio;
+        public bool TemAr;
+        public bool TemMP3;
+        
+        public decimal PrecoT
+        {
+            get
+            {
+                decimal preco = Choice.Preco;
+                if (TemAr) preco += Choice.ArCondicionadoValue;
+                if (TemFreio) preco += Choice.FreioABSValue;
+                if (TemMP3) preco += Choice.MP3PlayerValue;
+                return preco;
+            }
 
+            set
+            {
+                PrecoT = value;
+            }
+        }
+        public string PrecoStr
+        {
+            get
+            {
+                return String.Format("Total: R$ {0}",this.PrecoT);
+            }
+        }
+        
         public DetalheView (Veiculo veiculo)
 		{
 			InitializeComponent ();
             this.Choice = veiculo;
+            this.PrecoT = Choice.Preco;
             this.BindingContext = this;
 		}
 
